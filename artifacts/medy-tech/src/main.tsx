348c72e51a701a930117bc53e2e91e27eff5794b
yossefesam1541-wq <yossefesam1541@gmail.com>
2026-09-02 18:22:00 +0000

    عنهه

--- artifacts/medy-tech/src/main.tsx
+++ artifacts/medy-tech/src/main.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import { setBaseUrl } from '@workspace/api-client-react';
 
 import './index.css';
+
+setBaseUrl('https://megy-fasion3-api-server.vercel.app');
 
 createRoot(document.getElementById('root')!).render(<App />);
